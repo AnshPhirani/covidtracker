49fb8c922b1dba420cf92bcc526ce56763d216da
Ansh Phirani <anshphirani@gmail.com>
2022-06-23 00:11:50 +0530

    Initial commit

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@mui/material";
+import CountUp from "react-countup";
+
+import styles from "./Cards.module.css";
+import classNames from "classnames";
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loding...";
+  }
+
+  return (
+    <div className={styles.container}>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={classNames(styles.card, styles.infected)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography varaint="h5" component="h2">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography varaint="body2">
+              Number of Active Cases of Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={classNames(styles.card, styles.recovered)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography varaint="h5" component="h2">
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography varaint="body2">
+              Number of recoveries from Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={classNames(styles.card, styles.deaths)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography varaint="h5" component="h2">
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography varaint="body2">
+              Number of deaths cause by Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Cards;
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react'
+import {NativeSelect, FormControl} from "@mui/material"
+import { fetchCountries } from '../../api';
+
+import styles from "./CountryPicker.module.css";
+
+const CountryPicker = ({handleCountryChange}) => {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountries = async () => {
+      setCountries(await fetchCountries());
+    }
+
+    getCountries();
+
+  }, [setCountries])
+
+  return (
+    <FormControl className={styles.formcontrol}>
+      <NativeSelect defaultValue="" onChange={(e)=> {handleCountryChange(e.target.value)}}>
+        <option value="">Global</option>
+        {countries.map((country, i) => <option key = {i} value={country}>{country}</option> )}
+      </NativeSelect>
+    </FormControl>
+  )
+}
+
+export default CountryPicker
